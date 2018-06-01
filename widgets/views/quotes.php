c08291f2c60b11891b418dbc452bb4fc3d0ae5bd
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-06-01 09:31:01 +0200

    Update quotes.php

--- widgets/views/quotes.php
+++ widgets/views/quotes.php
@@ -1,34 +1,34 @@
 <?php
 
+use humhub\widgets\PanelMenu;
 use yii\helpers\Html;
 
 \humhub\modules\quotes\assets\Assets::register($this);
 ?>
 
 <div class="panel panel-default" id="quotes">
-    <?php echo \humhub\widgets\PanelMenu::widget(['id' => 'quotes']); ?>
+    <?= PanelMenu::widget(['id' => 'quotes']); ?>
 
     <div class="panel-heading">
-        <i class="fa fa-comment"></i>&nbsp;<?php echo $title; ?>
+        <i class="fa fa-comment"></i>&nbsp;<?= $title; ?>
     </div>
     <div class="panel-body">
-        <div id="owlRandom" class="owl-carousel" style="height:<?php echo $height; ?>px!important">
-      <span class="item"><span class="message"><?php echo $titlequote1; ?></span><br><br><span class="writer"><?php echo $titlewriterquote1; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote2; ?></span><br><br><span class="writer"><?php echo $titlewriterquote2; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote3; ?></span><br><br><span class="writer"><?php echo $titlewriterquote3; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote4; ?></span><br><br><span class="writer"><?php echo $titlewriterquote4; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote5; ?></span><br><br><span class="writer"><?php echo $titlewriterquote5; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote6; ?></span><br><br><span class="writer"><?php echo $titlewriterquote6; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote7; ?></span><br><br><span class="writer"><?php echo $titlewriterquote7; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote8; ?></span><br><br><span class="writer"><?php echo $titlewriterquote8; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote9; ?></span><br><br><span class="writer"><?php echo $titlewriterquote9; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote10; ?></span><br><br><span class="writer"><?php echo $titlewriterquote10; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote11; ?></span><br><br><span class="writer"><?php echo $titlewriterquote11; ?></span></span>
-      <span class="item"><span class="message"><?php echo $titlequote12; ?></span><br><br><span class="writer"><?php echo $titlewriterquote12; ?></span></span>
-</div>
+        <div id="owlRandom" class="owl-carousel" style="height:<?= $height; ?>px !important">
+            <span class="item"><span class="message"><?= $titlequote1; ?></span><br><br><span class="writer"><?= $titlewriterquote1; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote2; ?></span><br><br><span class="writer"><?= $titlewriterquote2; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote3; ?></span><br><br><span class="writer"><?= $titlewriterquote3; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote4; ?></span><br><br><span class="writer"><?= $titlewriterquote4; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote5; ?></span><br><br><span class="writer"><?= $titlewriterquote5; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote6; ?></span><br><br><span class="writer"><?= $titlewriterquote6; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote7; ?></span><br><br><span class="writer"><?= $titlewriterquote7; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote8; ?></span><br><br><span class="writer"><?= $titlewriterquote8; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote9; ?></span><br><br><span class="writer"><?= $titlewriterquote9; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote10; ?></span><br><br><span class="writer"><?= $titlewriterquote10; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote11; ?></span><br><br><span class="writer"><?= $titlewriterquote11; ?></span></span>
+            <span class="item"><span class="message"><?= $titlequote12; ?></span><br><br><span class="writer"><?= $titlewriterquote12; ?></span></span>
+        </div>
     </div>
 </div>
-
 
 <script>
 $(document).on('ready pjax:success', function() {
